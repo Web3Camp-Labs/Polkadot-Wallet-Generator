90b8f02ee2766d4db1b743bca513be63937e644c
wendyChaung <117485655@qq.com>
2022-03-01 20:48:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     styledComponents: true,
   },
+  assetPrefix: isProd ?'/ETH-Wallet-Generator':'',
 }
 
 module.exports = nextConfig
