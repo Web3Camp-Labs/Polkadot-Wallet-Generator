6228718620ba70a4b1805d5f3dd47d4f6da6f219
wendychaung <wendychaung@users.noreply.github.com>
2022-03-27 05:07:44 +0000

    deploy: 83dce0c6776c2df3abb41a9a3790d1097de977fc

---
+++ _next/static/8xq7hzLgJPpNTt7xR6PnL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/922edf34-655e50ae4f79e818.js","static/chunks/707-a74b90bcd9829fb1.js","static/chunks/pages/index-9c8be313cd5d4b4b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Aqf1JyCacuGt8YNGllhGt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/922edf34-655e50ae4f79e818.js","static/chunks/707-a74b90bcd9829fb1.js","static/chunks/pages/index-185ac7d09937a58e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
